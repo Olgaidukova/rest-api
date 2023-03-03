eba961a24cec72667e31519d54746d527b676505
Olgaidukova <olya.gaidukova@gmail.com>
2023-03-03 18:54:41 +0300

    add tests

---
+++ src/test/java/RegresInTests.java
@@ -0,0 +1,107 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class RegresInTests {
+
+    @DisplayName("Register-Successful")
+    @Test
+    void registerSuccessful() {
+
+        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("id", is(4),"token", is("QpwL5tke4Pnpja7X4"));
+
+    }
+    @DisplayName("Register without password")
+    @Test
+    void registerUnsuccessful() {
+
+        String data = "{ \"email\": \"eve.holt@reqres.in\"}";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+
+    }
+    @DisplayName("Register without email")
+    @Test
+    void registerUnsuccessfulWithoutEmail() {
+
+        String data = "{ \"password\": \"pistol\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+
+    }
+
+    @DisplayName("Register with empty email/password")
+    @Test
+    void registerUnsuccessfulWithEmptyData() {
+
+        String data = "{ \"email\": \"\", \"password\": \"\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+
+    }
+
+    @DisplayName("Successful update")
+    @Test
+    void updateJobSuccessful() {
+
+        String data = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"),"job", is("zion resident"));
+
+    }
+}
